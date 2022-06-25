848285c2e407b79dd90a150f5b78482af060587e
upluke <lukeforadream@gmail.com>
2022-06-24 23:45:47 -0700

    nodemon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express=require('express');
 const app= express()
+const port=process.env.PORT || 4000;
 
-app.get('/', (req, res)=> res.send('Hello Note'))
-app.listen(4000, ()=> console.log('Listening on port 4000.'))
+
+app.get('/', (req, res)=> res.send('Hello Note again'))
+
+
+app.listen(port, ()=> console.log(`Server running at http://localhost:${port}`))
